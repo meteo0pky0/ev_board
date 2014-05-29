9c199cd423090a4483d3c7520e04cfc0ec949dbf
meteo0pky0 <apxpgh@gmail.com>
2014-05-29 22:36:35 +0900

    그래고리적용

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '1';
+$db['default']['hostname'] = '';
 $db['default']['username'] = '';
 $db['default']['password'] = '!';
 $db['default']['database'] = '';
