725472427346a3186e2690e7b35b2cd3c68bb28a
meteo0pky0 <apxpgh@gmail.com>
2014-05-29 22:34:43 +0900

    복구

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '';
+$db['default']['hostname'] = '1';
 $db['default']['username'] = '';
 $db['default']['password'] = '!';
 $db['default']['database'] = '';
